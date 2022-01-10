e2b754a0c44abfb8c6188806105d096133720bc3
salehtz <m.s_ta@live.com>
2022-01-10 23:47:44 +0330

    Replaced sqflite with Hive.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'components/colors.dart';
+import 'model/notes_model.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory dir = await getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
+  Hive.registerAdapter<Note>(NoteAdapter());
+  await Hive.openBox<Note>('notes');
+
   runApp(const MyApp());
 }
 
