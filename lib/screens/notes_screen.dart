3aa1c3bf123911b811f3945477d35e423ab541b7
salehtz <m.s_ta@live.com>
2022-01-10 23:48:34 +0330

    Initial Commit.

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_app/components/colors.dart';
+import 'package:note_app/model/notes_model.dart';
+import 'package:note_app/screens/show_notes_screen.dart';
 
 class NotesScreen extends StatefulWidget {
   const NotesScreen({Key? key}) : super(key: key);
@@ -11,30 +15,52 @@ class NotesScreen extends StatefulWidget {
 
 class _NotesScreenState extends State<NotesScreen> {
   Size? _size;
-  String? _noteTitle, _noteDate;
-  int? _noteColor;
 
   @override
   Widget build(BuildContext context) {
     _size = MediaQuery.of(context).size;
-    _noteTitle = 'Title of the notes should be here.';
-    _noteDate = 'Feb 01,2021';
-    _noteColor = noteColors[0];
 
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: _size!.width * 0.03,
-        mainAxisSpacing: _size!.width * 0.03,
-      ),
-      itemBuilder: (context, index) {
-        return GridTile(
-          child: NotesTile(
-              size: _size,
-              noteTitle: _noteTitle,
-              noteDate: _noteDate,
-              noteColor: _noteColor),
-        );
+    final _globalKey = GlobalKey<_NotesScreenState>();
+
+    return ValueListenableBuilder(
+      valueListenable: Hive.box<Note>('notes').listenable(),
+      builder: (BuildContext context, Box<Note> box, _) {
+        if (box.length != 0) {
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: _size!.width * 0.03,
+              mainAxisSpacing: _size!.width * 0.03,
+            ),
+            itemCount: box.length,
+            itemBuilder: (context, index) {
+              final note = box.getAt(index);
+              return GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ShowNoteScreen(noteIndex: index)),
+                ),
+                child: GridTile(
+                  key: _globalKey,
+                  child: NotesTile(
+                      key: _globalKey,
+                      size: _size,
+                      noteTitle: note!.title,
+                      noteDate: note.date,
+                      noteColor: noteColors[note.color]),
+                ),
+              );
+            },
+          );
+        } else {
+          return const Center(
+            child: Text(
+              'Add some notes!',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+          );
+        }
       },
     );
   }
@@ -62,16 +88,20 @@ class NotesTile extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(_size!.width * 0.025),
       child: Container(
-        padding: EdgeInsets.all(_size!.width * 0.01),
+        padding: EdgeInsets.all(_size!.width * 0.02),
         color: Color(_noteColor!),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              _noteTitle!,
-              style: GoogleFonts.raleway(
-                fontSize: 20,
-                color: const Color(greyColor),
+            Flexible(
+              child: Text(
+                _noteTitle!,
+                overflow: TextOverflow.fade,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.raleway(
+                  fontSize: _size!.width * 0.1,
+                  color: const Color(greyColor),
+                ),
               ),
             ),
             Align(
@@ -79,7 +109,7 @@ class NotesTile extends StatelessWidget {
               child: Text(
                 _noteDate!,
                 style: GoogleFonts.inter(
-                    fontSize: 12,
+                    fontSize: _size!.width * 0.03,
                     color: const Color(greyColor).withOpacity(0.5)),
               ),
             ),
