80a3d96f2a97985a922c6555e6f91ff32c8df097
salehtz <m.s_ta@live.com>
2022-01-21 13:16:40 +0330

    Changed theme color.

--- lib/components/colors.dart
+++ lib/components/colors.dart
@@ -4,16 +4,16 @@ const purpleColor = 0xFF880E4F;
 const greyColor = 0xFF171717;
 
 Map<int, Color> appThemeColors = {
-  50: const Color.fromRGBO(136, 14, 79, .1),
-  100: const Color.fromRGBO(136, 14, 79, .2),
-  200: const Color.fromRGBO(136, 14, 79, .3),
-  300: const Color.fromRGBO(136, 14, 79, .4),
-  400: const Color.fromRGBO(136, 14, 79, .5),
-  500: const Color.fromRGBO(136, 14, 79, .6),
-  600: const Color.fromRGBO(136, 14, 79, .7),
-  700: const Color.fromRGBO(136, 14, 79, .8),
-  800: const Color.fromRGBO(136, 14, 79, .9),
-  900: const Color.fromRGBO(136, 14, 79, 1),
+  50: const Color.fromRGBO(216, 146, 22, .1),
+  100: const Color.fromRGBO(216, 146, 22, .2),
+  200: const Color.fromRGBO(216, 146, 22, .3),
+  300: const Color.fromRGBO(216, 146, 22, .4),
+  400: const Color.fromRGBO(216, 146, 22, .5),
+  500: const Color.fromRGBO(216, 146, 22, .6),
+  600: const Color.fromRGBO(216, 146, 22, .7),
+  700: const Color.fromRGBO(216, 146, 22, .8),
+  800: const Color.fromRGBO(216, 146, 22, .9),
+  900: const Color.fromRGBO(216, 146, 22, 1),
 };
 
 List<int> noteColors = [
